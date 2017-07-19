a12a0d0dfb1257ab7fa1550d4a1d0b713b4acbff
Damien Philippon <damien.philippon.dev@gmail.com>
2017-07-19 21:00:32 +0700

    Changing the script also is good sometimes

--- _scripts/indexing.sh
+++ _scripts/indexing.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-cat ./indexes/lunr.json | node build-index.js > ./indexes/index.json
-cat ./indexes/lunr.operators.json | node build-index.js > ./indexes/index_operator.json
-cat ./indexes/lunr.architectures.json | node build-index.js > ./indexes/index_architectures.json
-cat ./indexes/lunr.skills.json | node build-index.js > ./indexes/index_skills.json
-cat ./indexes/lunr.species.json | node build-index.js > ./indexes/index_species.json
-cat ./indexes/lunr.types.json | node build-index.js > ./indexes/index_types.json
-cat ./indexes/lunr.literals.json | node build-index.js > ./indexes/index_literals.json
-cat ./indexes/lunr.statements.json | node build-index.js > ./indexes/index_statements.json
-cat ./indexes/lunr.constants.json | node build-index.js > ./indexes/index_constants.json
+cat ./indexes/lunr.json | node ./_scripts/build-index.js > ./indexes/index.json
+cat ./indexes/lunr.operators.json | node ./_scripts/build-index.js > ./indexes/index_operator.json
+cat ./indexes/lunr.architectures.json | node ./_scripts/build-index.js > ./indexes/index_architectures.json
+cat ./indexes/lunr.skills.json | node ./_scripts/build-index.js > ./indexes/index_skills.json
+cat ./indexes/lunr.species.json | node ./_scripts/build-index.js > ./indexes/index_species.json
+cat ./indexes/lunr.types.json | node ./_scripts/build-index.js > ./indexes/index_types.json
+cat ./indexes/lunr.literals.json | node ./_scripts/build-index.js > ./indexes/index_literals.json
+cat ./indexes/lunr.statements.json | node ./_scripts/build-index.js > ./indexes/index_statements.json
+cat ./indexes/lunr.constants.json | node ./_scripts/build-index.js > ./indexes/index_constants.json
