7c40b8fa45b9b79b6d7a6db2c37fef4b13bf2214
Damien Philippon <damien.philippon.dev@gmail.com>
2017-07-19 17:39:25 +0700

    Changing link of generated index in the script

--- _scripts/indexing.sh
+++ _scripts/indexing.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-cat lunr.json | node build-index.js > index.json
-cat lunr.operators.json | node build-index.js > index_operator.json
-cat lunr.architectures.json | node build-index.js > index_architectures.json
-cat lunr.skills.json | node build-index.js > index_skills.json
-cat lunr.species.json | node build-index.js > index_species.json
-cat lunr.types.json | node build-index.js > index_types.json
-cat lunr.literals.json | node build-index.js > index_literals.json
-cat lunr.statements.json | node build-index.js > index_statements.json
-cat lunr.constants.json | node build-index.js > index_constants.json
+cat ../indexes/lunr.json | node build-index.js > index.json
+cat ../indexes/lunr.operators.json | node build-index.js > ../indexes/index_operator.json
+cat ../indexes/lunr.architectures.json | node build-index.js > ../indexes/index_architectures.json
+cat ../indexes/lunr.skills.json | node build-index.js > ../indexes/index_skills.json
+cat ../indexes/lunr.species.json | node build-index.js > ../indexes/index_species.json
+cat ../indexes/lunr.types.json | node build-index.js > ../indexes/index_types.json
+cat ../indexes/lunr.literals.json | node build-index.js > ../indexes/index_literals.json
+cat ../indexes/lunr.statements.json | node build-index.js > ../indexes/index_statements.json
+cat ../indexes/lunr.constants.json | node build-index.js > ../indexes/index_constants.json
