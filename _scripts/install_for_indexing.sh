8c33d0c2eded1650073aa7185f82802fe91df626
Damien Philippon <damien.philippon.dev@gmail.com>
2017-08-10 14:49:46 +0700

    Adding bundle install

--- _scripts/install_for_indexing.sh
+++ _scripts/install_for_indexing.sh
@@ -4,3 +4,4 @@ curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add 
 sudo sh -c "echo deb https://deb.nodesource.com/node_6.x yakkety main \ > /etc/apt/sources.list.d/nodesource.list"
 sudo apt-get install nodejs
 npm install lunr
+bundle install
