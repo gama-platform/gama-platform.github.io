9af9699b541f3e5d7db1360282ec5c5cc2afdd26
Arthur Brugiere <contact@arthurbrugiere.fr>
2019-08-16 15:32:15 +0700

    [TRAVIS] with the script it's better Sorry, I've forgot to add the script in my previous commit 🙊

---
+++ script/extendedTimeout.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# send the long living command to background
+GIT_USER="${GH_NAME}" yarn run publish-gh-pages &
+
+# Constants
+RED='\033[0;31m'
+minutes=0
+limit=3
+
+while kill -0 $! >/dev/null 2>&1; do
+  echo -n -e " \b" # never leave evidences!
+
+  if [ $minutes == $limit ]; then
+    echo -e "\n"
+    echo -e "${RED}Test has reached a ${minutes} minutes timeout limit"
+    exit 1
+  fi
+
+  minutes=$((minutes+1))
+
+  # Sleep 500 seconds ~ 10 minutes
+  sleep 500
+done
+
+exit 0
