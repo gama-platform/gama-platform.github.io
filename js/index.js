b3b08d84bcc01cf025eb03f1828acec9d3038d18
Gama <my.gama.bot@gmail.com>
2020-03-27 13:41:23 +0000

    Deploy website
    Deploy website version based on 6dd6e594194a49a5ded3076b30467aa3ad91c85e

--- js/index.js
+++ js/index.js
@@ -5,7 +5,3 @@
 // On page ready
 // -> Wait Fuse.js to be loaded
 document.addEventListener('DOMContentLoaded', setImageHeight, false);
-
-function setImageHeight(){
-	document.getElementById("imgSoft").style.maxHeight = 'calc(60vh - ' + 2 * document.getElementById("promo").offsetHeight+'px)';
-}
