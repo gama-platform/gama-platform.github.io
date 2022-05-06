a1cdac7f1907346be9bbd8f658aaa66b7773e9b8
RoiArthurB <contact@arthurbrugiere.fr>
2022-05-06 16:21:16 +0700

    [DOCv2] Init transition from v1 to v2

---
+++ website/.docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require('/home/roiarthurb/Documents/GAMA/gama-platform.github.io/website/node_modules/infima/dist/css/default/default.css'),
+  require('/home/roiarthurb/Documents/GAMA/gama-platform.github.io/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/home/roiarthurb/Documents/GAMA/gama-platform.github.io/website/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
+  require('/home/roiarthurb/Documents/GAMA/gama-platform.github.io/website/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/home/roiarthurb/Documents/GAMA/gama-platform.github.io/website/static/css/custom.css'),
+];
