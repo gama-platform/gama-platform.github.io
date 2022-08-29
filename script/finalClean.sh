02b409d80d22b799d107a5130376442159902abb
RoiArthurB <contact@arthurbrugiere.fr>
2022-08-29 14:37:25 +0700

    [CLEAN] Fix issue #118

--- script/finalClean.sh
+++ script/finalClean.sh
@@ -12,6 +12,8 @@ rm -fr "$( dirname "${BASH_SOURCE[0]}" )/../docs/G__BlankPage.md"
 rm -fr "$( dirname "${BASH_SOURCE[0]}" )/../docs/Operators.md"
 rm -fr "$( dirname "${BASH_SOURCE[0]}" )/../docs/OperatorsSplitted.md"
 rm -fr "$( dirname "${BASH_SOURCE[0]}" )/../docs/Index.md"
+# https://github.com/gama-platform/gama-platform.github.io/issues/118
+rm -fr "$( dirname "${BASH_SOURCE[0]}" )/../docs/UnitsAndConstantsPDF.md"
 
 # Verify every MD have a FM title, otherwise set one by default
 for f in $( dirname "${BASH_SOURCE[0]}" )/../docs/*.md ; do
