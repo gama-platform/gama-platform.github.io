fd5ac9ae835d88bdf1421a8c1e9391d84c8734e5
RoiArthurB <contact@arthurbrugiere.fr>
2023-02-02 11:50:34 +0700

    [FRONT] Update front page - Fix #133

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,110 +1,124 @@
 /**
- * Copyright (c) 2019-present, Arthur Brugiere, GAMA-Platform
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
- * This source code is licensed under the GPL3 license found in the
+ * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
 
 import React from 'react';
+import clsx from 'clsx';
 import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './index.module.css';
 
-/*  FIRST MAIN BLOCK  */
-function HomeSplash() { //} extends React.Component {
-  const {siteConfig} = useDocusaurusContext();
-  const docUrl = doc => `${useBaseUrl("wiki")}/${doc}`
-
-  const FlexContainer = props => ( <div className="container"> {props.children} </div> );
-  const FlexItem = props => ( <div className={`col${props.size} ${props.className}`}>{props.children}</div> );
-
-  const Button = props => (
-    <a className={props.className} href={props.href} target={props.target} style={{fontSize: "24px", margin: "0 .5em"}} >
-        {props.children}
-      </a>
-  );
+function Home() {
+  const context = useDocusaurusContext();
+  const {siteConfig} = context;
 
   return (
-    <div>
-
-      <div className="row firstLine">
-        <FlexItem className="" size="--12 col">
-            <div className="" style={{textAlign: "center"}} >
-
-              <a href={`${useBaseUrl("download")}`} >
-                <img id="imgSoft" src="/img/gama-logo.png" alt="GAMA-Platform Software" style={{maxHeight: "280px"}} />
-              </a>
+    <Layout
+      title={siteConfig.title}
+      description="A modern styling framework for content-driven websites">
+      <header className={clsx('hero', styles.heroBanner)}>
+        <div className="container">
+          <img
+            className={clsx(styles.heroBannerLogo, 'margin-vert--md')}
+            src={useBaseUrl('/img/gama-logo.png')}
+          />
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">GAMA is a modeling and simulation development environment for building spatially explicit agent-based simulations</p>
+          <div className={styles.buttons}>
+            <Link
+              style={{margin: "0 auto"}}
+              className={clsx(
+                'button button--warning button--lg',
+                styles.getStarted,
+              )}
+              to={useBaseUrl('download')}>
+              Download
+            </Link>
+            
+            <Link
+              style={{margin: "0 auto"}}
+              className={clsx(
+                'button button--outline button--danger button--lg',
+                styles.getStarted,
+              )}
+              to={useBaseUrl('wiki/Home')}>
+              Documentation
+            </Link>
+            
+            <Link
+              style={{margin: "0 auto"}}
+              className={clsx(
+                'button button--outline button--primary button--lg',
+                styles.getStarted,
+              )}
+              to={useBaseUrl('wiki/Tutorials')}>
+              Tutorials
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        <div className={styles.sectionDark}>
+          <div className="container padding-vert--md">
+            <div className="row">
+              <div className="col col--8 col--offset-2">
+                <div className="margin-vert--lg text--center">
+                  <h2 className={styles.sectionDarkTitle}>
+                    Take a look at the latest <a href="https://www.youtube.com/watch?v=jpWxk1hcTTk&list=PLIf4h7zpeBeHrK7GY7HeWojXniBU_6nN-" target="_blank">international GAMA Days 2022 replay</a>
+                  </h2>
+                </div>
+              </div>
             </div>
-        </FlexItem>
-        <FlexItem className="" size="--12 col">
-          <div style={{paddingTop: "4em", textAlign: "center"}} >
-            <h1 className="hero__title">{siteConfig.title}</h1>
-            <p className="hero__subtitle" >GAMA is a modeling and simulation development environment<br/>for building spatially explicit agent-based simulations.</p>
           </div>
-        </FlexItem>
-
-          <div className="col col--12" style={{"textAlign": "center", margin: "3em auto"}} >
-            <Button href={`${useBaseUrl("download")}`}  className="button button--warning">Download</Button>
-            <Button href={docUrl('Home')} className="button button--outline button--danger">Documentation</Button>
-              <Button href={docUrl('Tutorials')} className="button button--outline button--primary">Tutorials</Button>
-          </div>
-       </div>
-
-    </div>
-  );
-}
-
-export default function Hello() {
-  const {siteConfig} = useDocusaurusContext();
-
-  const Design = props => (    
-    <style dangerouslySetInnerHTML={{__html: `
-      .row { padding-top: 10vh; }
-      .firstLine .col{ margin: 0 3em; }
-      footer { display: none; }
-      .button { border-width: 2px;font-weight: bold; }
-      #back-to-top { display: none; }
-      body{ overflow: hidden; }
-      #background.container { position: relative; }
-      .bgGif {
-        opacity: 0.6;
-        position: absolute;
-        left: 0;
-        top: 0;
-        width: 100%;
-        height: auto;
-        display: block;
-        margin-bottom:auto;
-        margin-top: 10vh;
-      }
-      #background > .row {
-        position: relative;
-        paddingTop: "0";
-        z-index = 2;
-      }
-
-      @media only screen and (max-width: 600px) {
-        img.bgGif {
+        </div>
+        <p className="padding-vert--xl">
+          <p className="container">
+            <p className="row">
+              <p className="col col--10 col--offset-1">
+                <h2 className="text--center margin-bottom--xl">Why the GAMA platform?</h2>
+                <div className="row margin-vert--lg">
+                  <div className="col">
+                    <h3><i class="fa-brands fa-osi"></i> Open Source</h3>
+                    <p>We are entirely dedicated to Free and Open Source Software. All of our utilities are licensed under the GNU General Public License (v3).</p>
+                  </div>
+                  <div className="col">
+                    <h3><i class="fa-solid fa-flask"></i> Scientific Tools</h3>
+                    <p>GAMA is used by multiple scientific projects and cited in hundreds of scientific articles.</p>
+                  </div>
+                  <div className="col">
+                    <h3><i class="fa-solid fa-map"></i> GIS support</h3>
+                    <p>
+                      GAMA have a native GIS support to easily apply your model on real life geographic data.
+                    </p>
+                  </div>
+                </div>
+                <div className="row margin-vert--lg">
+                  <div className="col">
+                    <h3><i class="fa-solid fa-rocket"></i> Simply powerful</h3>
+                    <p>GAMA uses an intuitive programming language (GAML)  developed to be used by non-computer scientists.<br/><a href="https://www.youtube.com/watch?v=YGHw1LSzd-E" target="_blank">Discover it in 10 minutes!</a></p>
+                  </div>
+                  <div className="col">
+                    <h3><i class="fa-solid fa-download"></i> Cross Platform</h3>
+                    <p>GAMA can be <a href="download">installed on all operating systems</a> that support Java, i.e, Windows, Linux and Mac OSX.<br/>Write Once, Run Anywhere.</p>
+                  </div>
+                </div>
+              </p>
+            </p>
+          </p>
+        </p>
+      </main>
+      <style dangerouslySetInnerHTML={{__html: `
+        footer.footer {
           display: none;
         }
-        #background > .row {
-          z-index = 0;
-        }
-      }
-
-    `}} />
-  );
-
-  return (
-    <Layout>
-      <Design/>
-
-      <div id="background" className="container">
-        <img className="bgGif" src="/img/GAMA_1.8.2_transparent.gif" alt="" />
-        <div className="row">
-          <HomeSplash />
-        </div>
-      </div>  
+      `}} />
     </Layout>
   );
 }
+
+export default Home;
